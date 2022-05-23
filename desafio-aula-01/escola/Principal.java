4bf0b891ae152dd9f5ef7efc56508a69a810494d
Indiamara Enes <indiamaraenes@gmail.com>
2022-05-23 20:08:18 -0300

    corrigindo descrição

--- desafio-aula-01/escola/Principal.java
+++ desafio-aula-01/escola/Principal.java
@@ -5,11 +5,11 @@ public class Principal {
     public static void main(String[] args) {
         Aluno aluno = new Aluno();
 
-        aluno.nome = "Jorge Campos";
+        aluno.nome = "Heloisa Costa";
         aluno.matricula = "00221133";
         aluno.endereco = "Rua das rosas";
         aluno.telefone = "00-0000-0000";
-        aluno.email = "jorgecampos@seudominio.com";
+        aluno.email = "helo@seudominio.com";
 
         System.out.println("Nome: " + aluno.nome + " Matrícula: " + aluno.matricula + " Endereço: " +
                 aluno.endereco + " Telefone: " + aluno.telefone + " E-mail: " + aluno.email);
