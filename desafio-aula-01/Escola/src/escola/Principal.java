b04729666efa0caf8f74f12327986c934c4a684e
Indiamara Enes <indiamaraenes@uniopet.edu.br>
2023-07-13 23:56:40 -0300

    Atualizar o Principal.java
    Adicionado lista de alunos

--- desafio-aula-01/Escola/src/escola/Principal.java
+++ desafio-aula-01/Escola/src/escola/Principal.java
@@ -1,21 +1,23 @@
-
 package escola;
 
 public class Principal {
-    public static void main(String [] args){
-    Aluno aluno = new Aluno();
-    
-    aluno.nome = "Jorge Campos";
-    aluno.matricula = "00221133";
-    aluno.endereco = "Rua das rosas";
-    aluno.telefone = "00-0000-0000";
-    aluno.email = "fulano@dominio.com";
-    
-    
-    System.out.println("Nome: " + aluno.nome + " Matrícula: " + aluno.matricula+ " Endereço: " +
-           aluno.endereco+ " Telefone: " + aluno.telefone+ " E-mail: " + aluno.email);
-    
-    
+
+    public static void main(String[] args) {
+        // Cria uma lista de alunos
+        List<Aluno> alunos = new ArrayList<Aluno>();
+
+        // Adiciona um aluno à lista
+        Aluno aluno = new Aluno();
+        aluno.nome = "Jorge Campos";
+        aluno.matricula = "00221133";
+        aluno.endereco = "Rua das rosas";
+        aluno.telefone = "00-0000-0000";
+        aluno.email = "fulano@dominio.com";
+        alunos.add(aluno);
+
+        // Imprime os dados dos alunos
+        for (Aluno a : alunos) {
+            System.out.println("Nome: " + a.nome + " Matrícula: " + a.matricula + " Endereço: " + a.endereco + " Telefone: " + a.telefone + " E-mail: " + a.email);
+        }
     }
-    
 }
